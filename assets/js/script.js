19bf767f2d1e2ed9b38a47667edb6056c85e600f
David Lucio <david.a.lucio@gmail.com>
2021-07-07 01:57:04 -0700

    It's doin' something. So that's a relief.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 var apiKey = "67441c74d353ce8e981d3bf51bb93d38";
 var apiURL = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=`;
+var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=imperial&q=`;
 
 function checkWeather(location) {
     var queryURL = apiURL + encodeURIComponent(location);
@@ -19,15 +20,29 @@ function checkWeather(location) {
                 // CITY NOT FOUND
                 alert("No city by that name was found");
             }
-        })
-        .then( function(data){
-
-            console.log(data);
-        
         });
 
 }
 
+function checkForecast(location) {
+    var queryURL = forecastURL + encodeURIComponent(location);
+
+    fetch(queryURL)
+        .then( function(response){
+            if(response.status == 200){
+                // CITY FOUND, LOADING DATA
+                $("input#cityname").val("");
+                response.json().then( function(data){
+                    displayCurrentForecast(location, data);
+                })
+            }
+            else{
+                // CITY NOT FOUND
+                alert("No city by that name was found");
+            }
+        });
+
+}
 
 /** DISPLAY FUNCTIONALITY **/
 var weatherWidget = $("#weatherwidget");
@@ -36,9 +51,25 @@ var historyWidget = $("#searchhistory");
 function displayCurrentWeather(city, data){
     
     weatherWidget.removeClass('nocity');
+
+    var currentReport = weatherWidget.find('#currentcity');
+    var fiveDayForecast = weatherWidget.find('#forecast');
     
-    var dataToString = JSON.stringify(data);
-    weatherWidget.find('#currentcity').html(`<code>${dataToString}</code>`);
+    currentReport.find('h3').text(data.name);
+    currentReport.find('#displayWeather span').text(data.weather[0].main);
+    currentReport.find('#displayTemp span').text(data.main.temp);
+    currentReport.find('#displayWind span').text(data.wind.speed);
+    currentReport.find('#displayHumidity span').text(data.main.humidity);
+
+    // SAVE TO LOCAL STORAGE?
+
+    // Populate the forecast area
+    checkForecast(data.name);
+}
+
+function displayCurrentForecast(city, data){
+
+    console.log(data);
 
 }
 
